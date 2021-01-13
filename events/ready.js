6ba115d276dde8aa43f36353cdefc4ccb56f9618
Dragneel <sinhasourav391@gmail.com>
2021-01-13 20:33:53 +0530

    activity

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,9 @@
 module.exports = async (client) => {
     console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
 
-    client.user.setActivity(client.config.discord.activity);
+    client.user.setActivity(`w!help | ver 1.0 music system`,{
+    type: "STREAMING",
+    url:'https://www.twitch.tv/.',
+
+    });
 };
