8245448b2eb3e82b4ed78eaf2a9cecd52791de9f
Dragneel <sinhasourav391@gmail.com>
2021-06-10 12:32:57 +0530

    kj

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -14,7 +14,7 @@ module.exports = {
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return errorembed(`**${client.emotes.error} Be in the same \`VC\` i connect to !**`,message.channel);
 
-        if (!client.player.getQueue(message)) return errorembed (`${client.emotes.error} Nope, nothing played in music queue! what to pause!**`,message.channel);
+        if (!client.player.getQueue(message)) return errorembed (`**${client.emotes.error} Nope, nothing played in music queue! what to pause!**`,message.channel);
 
         if (client.player.getQueue(message).paused) return corembed(`${client.emotes.error}  The music, it's already paused !`);
 
