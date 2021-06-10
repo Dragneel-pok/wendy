2e9890fc4334d19d7f2d0f33763bb41bb1cdf05c
Dragneel <sinhasourav391@gmail.com>
2021-06-10 09:40:20 +0530

    k

--- commands/music/play.js
+++ commands/music/play.js
@@ -10,9 +10,9 @@ module.exports = {
     execute(client, message, args) {
         if (!message.member.voice.channel) return errorembed(`**${client.emotes.error} Please join a voice channel where I have access <3**!`,message.channel);
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return errorembed(`${client.emotes.error} Be in the same \`VC\` i connect to !*`,message.channel);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return errorembed(`**${client.emotes.error} Be in the same \`VC\` i connect to !**`,message.channel);
 
-        if (!args[0]) return corembed(`${client.emotes.error}  Please indicate the title of a song !`.message.channel);
+        if (!args[0]) return corembed(`**${client.emotes.error}  Please indicate the title of a song !**`.message.channel);
 
         client.player.play(message, args.join(" "), { firstResult: true });
     },
